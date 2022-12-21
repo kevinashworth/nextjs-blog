0ce77ee08b87ad9e468533055486b4dd8f0e7180
Kevin Ashworth <kevinashworth@yahoo.com>
2022-12-21 11:02:43 -0800

    Small edit

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>K-Dawg is magnificent.</p>
+        <p>K-Dawg is magnificent. (Vercel)</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
